d3dd080a758fd3997f5a719a4905e9a7acf3f216
Danilo-Pereira-of <danilosantosadm95@gmail.com>
2023-05-09 12:36:00 -0300

    Project-Playstation-Store

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuList = document.getElementById("menu-list")
+menuList.style.maxHeight = "0px"
+
+function showMenu(){
+    if (menuList.style.maxHeight == "0px"){
+        menuList.style.maxHeight = "150px"
+
+    }else{
+        menuList.style.maxHeight = "0px"
+    }
+    
+}
